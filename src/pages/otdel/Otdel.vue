f9dbd9fb5c84e580c79babb1e1366ad79047f6fc
kotvasili <kot_vasili@mail.ru>
2017-07-14 17:49:16 +0300

    otdel

--- src/pages/otdel/Otdel.vue
+++ src/pages/otdel/Otdel.vue
@@ -31,7 +31,7 @@
               </md-dialog-content>
               <md-dialog-actions>
                 <md-button class="md-primary" @click.native="closeDialog('dialogLoad');">Отмена</md-button>
-                <md-button class="md-primary" @click.native="openDialog('loadStudentDialog');closeDialog('dialogLoad');">Загрузить</md-button>
+                <md-button class="md-primary" @click.native="openDialog('loadStudentDialog');closeDialog('dialogLoad');">Продолжить</md-button>
               </md-dialog-actions>
             </md-dialog>
 
@@ -56,7 +56,7 @@
               </md-dialog-content>
               <md-dialog-actions>
                 <md-button class="md-primary" @click.native="closeDialog('dialogPrepod');">Отмена</md-button>
-                <md-button class="md-primary" @click.native="openDialog('loadPrepodDialog');closeDialog('dialogPrepod');">Загрузить</md-button>
+                <md-button class="md-primary" @click.native="openDialog('loadPrepodDialog');closeDialog('dialogPrepod');">Продолжить</md-button>
               </md-dialog-actions>
             </md-dialog>
 
@@ -86,6 +86,197 @@
               </md-dialog-actions>
             </md-dialog>
           </div>
+        </md-layout>
+      </md-layout>
+        <md-layout>
+        <md-layout  md-flex="33">
+          <div class="phone-viewport">
+            <md-toolbar>
+              <span class="md-title">Преподаватели</span>
+            </md-toolbar>
+            <md-list>
+              <md-list-item>
+                  <span>Солодарь Василий Александрович</span>
+                  <md-list-expand>
+                  <div class="group-lists">
+                    <p class="md-subheading">Поток 2017</p>
+                    <div class="group-lists-item">
+                      <p class="md-body-2">Специальность: ИТУТС</p>
+                      <md-list>
+                          <md-list-item class="md-inset">482472</md-list-item>
+                          <md-list-item class="md-inset">482472</md-list-item>
+                          <md-list-item class="md-inset">482472</md-list-item>
+                          <md-list-item class="md-inset">482472</md-list-item>
+                      </md-list>
+                    </div>
+                    <div class="group-lists-item">
+                      <p class="md-body-2">Специальность: ИТУТС</p>
+                      <md-list>
+                          <md-list-item class="md-inset">482472</md-list-item>
+                          <md-list-item class="md-inset">482472</md-list-item>
+                          <md-list-item class="md-inset">482472</md-list-item>
+                          <md-list-item class="md-inset">482472</md-list-item>
+                      </md-list>
+                    </div>
+                  </div>
+                  <div class="group-lists">
+                    <p class="md-subheading">Поток 2016</p>
+                    <div class="group-lists-item">
+                      <p class="md-body-2">Специальность: ИТУТС</p>
+                      <md-list>
+                          <md-list-item class="md-inset">482472</md-list-item>
+                          <md-list-item class="md-inset">482472</md-list-item>
+                          <md-list-item class="md-inset">482472</md-list-item>
+                          <md-list-item class="md-inset">482472</md-list-item>
+                      </md-list>
+                    </div>
+                  </div>
+                  </md-list-expand>
+              </md-list-item>
+              <md-list-item>
+                  <span>Солодарь Василий Александрович</span>
+                  <md-list-expand>
+                  <div class="group-lists">
+                    <p class="md-subheading">Поток 2017</p>
+                    <div class="group-lists-item">
+                      <p class="md-body-2">Специальность: ИТУТС</p>
+                      <md-list>
+                          <md-list-item class="md-inset">482472</md-list-item>
+                          <md-list-item class="md-inset">482472</md-list-item>
+                          <md-list-item class="md-inset">482472</md-list-item>
+                          <md-list-item class="md-inset">482472</md-list-item>
+                      </md-list>
+                    </div>
+                    <div class="group-lists-item">
+                      <p class="md-body-2">Специальность: ИТУТС</p>
+                      <md-list>
+                          <md-list-item class="md-inset">482472</md-list-item>
+                          <md-list-item class="md-inset">482472</md-list-item>
+                          <md-list-item class="md-inset">482472</md-list-item>
+                          <md-list-item class="md-inset">482472</md-list-item>
+                      </md-list>
+                    </div>
+                  </div>
+                  <div class="group-lists">
+                    <p class="md-subheading">Поток 2016</p>
+                    <div class="group-lists-item">
+                      <p class="md-body-2">Специальность: ИТУТС</p>
+                      <md-list>
+                          <md-list-item class="md-inset">482472</md-list-item>
+                          <md-list-item class="md-inset">482472</md-list-item>
+                          <md-list-item class="md-inset">482472</md-list-item>
+                          <md-list-item class="md-inset">482472</md-list-item>
+                      </md-list>
+                    </div>
+                  </div>
+                  </md-list-expand>
+              </md-list-item>
+              </md-list>
+          </div>
+        </md-layout>
+        <md-layout md-vertical-align="start" md-flex="66">
+          <div class="groups-odtel-list">
+            <md-toolbar>
+              <span class="md-title">Группы</span>
+            </md-toolbar>
+            <md-table-card>
+            <md-list>
+              <md-list-item>
+                  <md-toolbar md-theme="white">
+                    <h1 class="md-title">Поток 2016</h1>
+                  </md-toolbar>
+                  <md-list-expand>
+                    <div class="groups-odtel-elem">
+                    <md-table v-once>
+                      <md-table-header>
+                        <md-table-row>
+                          <md-table-head>№ группы</md-table-head>
+                          <md-table-head md-numeric>Специальность</md-table-head>
+                          <md-table-head md-numeric>Ответственный преподователь</md-table-head>
+                          <md-table-head md-numeric>Число студентов</md-table-head>
+                          <md-table-head md-numeric>Число отчетов получено</md-table-head>
+                        </md-table-row>
+                      </md-table-header>
+
+                      <md-table-body>
+                        <md-table-row v-for="(row, index) in 5" :key="index">
+                          <md-table-cell>468623</md-table-cell>
+                          <md-table-cell>ИТУТС</md-table-cell>
+                          <md-table-cell>Солодарь Василий Александрович</md-table-cell>
+                          <md-table-cell>14</md-table-cell>
+                          <md-table-cell>10/14</md-table-cell>
+                        </md-table-row>
+                      </md-table-body>
+                    </md-table>
+                    </div>
+                </md-list-expand>
+              </md-list-item>
+              <md-list-item>
+                  <md-toolbar md-theme="white">
+                    <h1 class="md-title">Поток 2017</h1>
+                  </md-toolbar>
+                  <md-list-expand>
+
+                    <md-table v-once>
+                      <md-table-header>
+                        <md-table-row>
+                          <md-table-head>№ группы</md-table-head>
+                          <md-table-head md-numeric>Специальность</md-table-head>
+                          <md-table-head md-numeric>Ответственный преподователь</md-table-head>
+                          <md-table-head md-numeric>Число студентов</md-table-head>
+                          <md-table-head md-numeric>Число отчетов получено</md-table-head>
+                        </md-table-row>
+                      </md-table-header>
+
+                      <md-table-body>
+                        <md-table-row v-for="(row, index) in 10" :key="index">
+                          <md-table-cell>468623</md-table-cell>
+                          <md-table-cell>ИТУТС</md-table-cell>
+                          <md-table-cell>Солодарь Василий Александрович</md-table-cell>
+                          <md-table-cell>14</md-table-cell>
+                          <md-table-cell>10/14</md-table-cell>
+                        </md-table-row>
+                      </md-table-body>
+                    </md-table>
+                </md-list-expand>
+              </md-list-item>
+              <md-list-item>
+                  <md-toolbar md-theme="white">
+                    <h1 class="md-title">Поток 2018</h1>
+                  </md-toolbar>
+                  <md-list-expand>
+                   <md-list>
+                    <md-list-item class="md-inset">
+                    <md-table v-once>
+                      <md-table-header>
+                        <md-table-row>
+                          <md-table-head>№ группы</md-table-head>
+                          <md-table-head md-numeric>Специальность</md-table-head>
+                          <md-table-head md-numeric>Ответственный преподователь</md-table-head>
+                          <md-table-head md-numeric>Число студентов</md-table-head>
+                          <md-table-head md-numeric>Число отчетов получено</md-table-head>
+                        </md-table-row>
+                      </md-table-header>
+
+                      <md-table-body>
+                        <md-table-row v-for="(row, index) in 5" :key="index">
+                          <md-table-cell>468623</md-table-cell>
+                          <md-table-cell>ИТУТС</md-table-cell>
+                          <md-table-cell>Солодарь Василий Александрович</md-table-cell>
+                          <md-table-cell>14</md-table-cell>
+                          <md-table-cell>10/14</md-table-cell>
+                        </md-table-row>
+                      </md-table-body>
+                    </md-table>
+                    </md-list-item>
+                   </md-list>
+                </md-list-expand>
+              </md-list-item>
+            </md-list>
+             </md-table-card>
+          </div>
+
+
         </md-layout>
       </md-layout>
     </div>
@@ -150,4 +341,51 @@ export default {
 /*.load-btn-wrap button{
   width: 350px;
 }*/
+.phone-viewport{
+  width: 400px;
+}
+.group-lists{
+  background-color: #fff;
+  text-align: left;
+}
+.group-lists > p{
+  background-color: #fff;
+  margin-top: 0;
+  padding-top: 20px;
+
+}
+.group-lists p{
+  padding-left: 30px;
+  margin-bottom: 5px;
+}
+.group-lists .group-lists-item p{
+  padding-left: 30px;
+  margin-bottom: 5px;
+  margin-top: 5px;
+}
+.groups-odtel-list{
+  width: 100%;
+}
+.groups-odtel-list .md-toolbar{
+  width: 100%;
+}
+.groups-odtel-list .md-list-item-expand{
+  min-height: 62px;
+}
+.groups-odtel-list .md-list-item-container{
+ padding-left: 0 !important;
+ padding-right: 0 !important;
+}
+.groups-odtel-list .md-table{
+   width: 100% !important;
+   text-align: left !important;
+}
+.groups-odtel-list .md-list-item .md-icon{
+   margin-right: 15px;
+}.groups-odtel-list .md-table-head-container{
+   text-align: left;
+}
+.groups-odtel-list .md-table{
+   background-color: #fff;
+}
 </style>
